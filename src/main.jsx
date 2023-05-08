de6b76a649327d0227f9c46670e94239b4d06379
byeonkho <byeon.kho@gmail.com>
2023-05-08 09:44:02 +0800

    set up scaffolding (see desc)
    app setup using npm vite@latest
    
    installed libraries:
    
    - react-big-calendar
    - MUI
    
    setup main MUI theme using provided FIGMA colour scheme

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@emotion/react";
+import Theme from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<ThemeProvider theme={Theme}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
