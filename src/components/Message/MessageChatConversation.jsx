43e5e540cd532c1f7ba89da1305d432eff2d1ed7
jovylim <jovylimwenjing@gmail.com>
2023-05-17 15:14:20 +0800

    convo aligns left and right

--- src/components/Message/MessageChatConversation.jsx
+++ src/components/Message/MessageChatConversation.jsx
@@ -1,10 +1,177 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
 
 const MessageChatConversation = (props) => {
+  const [senderNameAndPic, setSenderNameAndPic] = useState({});
+  const [receiverNameAndPic, setReceiverNameAndPic] = useState({});
+  const getNameAndPic = async () => {
+    const res = await fetch(
+      import.meta.env.VITE_SERVER + "/bump/users/namepiconly/namepiconly",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          uuid: props.senderUUID,
+        }),
+      }
+    );
+    if (res.status === 200) {
+      const data = await res.json();
+      setSenderNameAndPic(data);
+    } else {
+      alert("an error has occured at getting sender name and pic via uuid");
+    }
+
+    const res2 = await fetch(
+      import.meta.env.VITE_SERVER + "/bump/users/namepiconly/namepiconly",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          uuid: props.receiverUUID,
+        }),
+      }
+    );
+    if (res2.status === 200) {
+      const data = await res2.json();
+      setReceiverNameAndPic(data);
+    } else {
+      alert("an error has occured at getting receiver name and pic via uuid");
+    }
+  };
+  useEffect(() => {
+    getNameAndPic();
+  }, []);
+
+  useEffect(() => {
+    getNameAndPic();
+  }, [props.receiverUUID]);
+
   return (
     <>
-      <div>populate chat data here...</div>
+      {/* <div>{senderNameAndPic.name}</div>
+      <div>{senderNameAndPic.profilePicture}</div>
+      <div>{receiverNameAndPic.name}</div>
+      <div>{receiverNameAndPic.profilePicture}</div>
+      <div>{props.receiverUUID}</div> */}
+      <Container>
+        <Container
+          sx={{
+            display: "flex",
+            // positions: "fixed", top: "0"
+          }}
+        >
+          <Avatar
+            src={`../../../../pictures/${receiverNameAndPic.profilePicture}`}
+          ></Avatar>
+          <Typography
+            sx={{
+              mt: "2px",
+              ml: "15px",
+              fontSize: "25px",
+              fontWeight: "bold",
+            }}
+          >
+            {receiverNameAndPic.name}
+          </Typography>
+        </Container>
+        <Container
+          sx={{
+            height: "550px",
+            mt: "10px",
+            overflow: "hidden",
+            overflowY: "scroll",
+          }}
+        >
+          {props.filteredChatData.map((item) => {
+            if (item.senderUUID === props.senderUUID) {
+              return (
+                <>
+                  <Container
+                    sx={{
+                      pt: "10px",
+                      pb: "10px",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        borderWidth: "1px",
+                        borderStyle: "solid",
+                        borderColor: "black",
+                        borderRadius: "8px",
+                        mt: "5px",
+                        pl: "15px",
+                        pr: "15px",
+                        ml: "80px",
+                      }}
+                    >
+                      {item.message}
+                    </Typography>
+
+                    <Typography
+                      sx={{
+                        mt: "5px",
+                        ml: "290px",
+                      }}
+                    >
+                      {item.timesent.slice(0, 16).replace("T", " ")}
+                    </Typography>
+                    <Avatar
+                      src={`../../../../pictures/${senderNameAndPic.profilePicture}`}
+                      sx={{
+                        mt: "5px",
+                        ml: "380px",
+                      }}
+                    ></Avatar>
+                  </Container>
+                </>
+              );
+            } else {
+              return (
+                <>
+                  <Container
+                    sx={{
+                      pt: "10px",
+                      pb: "10px",
+                    }}
+                  >
+                    <Typography
+                      sx={{
+                        borderWidth: "1px",
+                        borderStyle: "solid",
+                        borderColor: "black",
+                        borderRadius: "8px",
+                        mt: "5px",
+                        pl: "15px",
+                        pr: "15px",
+                        mr: "80px",
+                      }}
+                    >
+                      {item.message}
+                    </Typography>
+
+                    <Typography
+                      sx={{
+                        mt: "5px",
+                        mr: "290px",
+                      }}
+                    >
+                      {item.timesent.slice(0, 16).replace("T", " ")}
+                    </Typography>
+                    <Avatar
+                      src={`../../../../pictures/${receiverNameAndPic.profilePicture}`}
+                      sx={{
+                        mt: "5px",
+                        mr: "380px",
+                      }}
+                    ></Avatar>
+                  </Container>
+                </>
+              );
+            }
+          })}
+        </Container>
+      </Container>
     </>
   );
 };
