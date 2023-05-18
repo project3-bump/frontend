a791cda571b10e7c691dfbba95556b3db8790ee7
jovylim <jovylimwenjing@gmail.com>
2023-05-18 10:00:30 +0800

    updates on css

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,86 +7,74 @@ import PulseOverview from "./PulseOverview/PulseOverview";
 import { fetchData } from "../../helpers/common";
 
 const Dashboard = (props) => {
-	const [userMoods, setUserMoods] = useState([]);
+  const [userMoods, setUserMoods] = useState([]);
+  const [moodSelected, setMoodSelected] = useState(false);
 
-	const getUserMoods = async () => {
-		const { ok, data } = await fetchData(
-			"/bump/users/oneuser",
-			undefined,
-			"PUT",
-			{
-				id: props.id,
-			}
-		);
+  const getUserMoods = async () => {
+    const { ok, data } = await fetchData(
+      "/bump/users/oneuser",
+      undefined,
+      "PUT",
+      {
+        id: props.id,
+      }
+    );
 
-		if (ok) {
-			setUserMoods(data);
-			console.log("user mood set");
-			console.log(data);
-		} else {
-			console.log("get user moods failed");
-		}
-	};
+    if (ok) {
+      setUserMoods(data);
+      console.log("user mood set");
+      console.log(data);
+    } else {
+      console.log("get user moods failed");
+    }
+  };
 
-	useEffect(() => {
-		getUserMoods();
-	}, []);
+  useEffect(() => {
+    getUserMoods();
+  }, []);
 
-	return (
-		<>
-			<Grid
-				container
-				justifyContent="flex-start"
-			>
-				{/* this grid item contains moodselector and pulse overview. */}
-				<Grid
-					item
-					xs={7}
-				>
-					<Grid
-						container
-						spacing="5px"
-						direction="column"
-						alignItems="left"
-					>
-						<Grid item>
-							<MoodSelector
-								id={props.id}
-								getUserMoods={getUserMoods}
-							/>
-						</Grid>
-						<Grid item>
-							<PulseOverview
-								todaysDateState={props.todaysDateState}
-								id={props.id}
-								userMoods={userMoods}
-							/>
-						</Grid>
-					</Grid>
-				</Grid>
+  useEffect(() => {
+    getUserMoods();
+    setMoodSelected(false);
+  }, [moodSelected]);
 
-				{/* this grid item contains today's calendar and notiifcations. */}
-				<Grid
-					item
-					xs={5}
-				>
-					<Grid
-						container
-						spacing="5px"
-						direction="column"
-						alignItems="center"
-					>
-						<Grid item>
-							<DateCard todaysDateState={props.todaysDateState} />
-						</Grid>
-						<Grid item>
-							<Notifications />
-						</Grid>
-					</Grid>
-				</Grid>
-			</Grid>
-		</>
-	);
+  return (
+    <>
+      <Grid container justifyContent="flex-start">
+        {/* this grid item contains moodselector and pulse overview. */}
+        <Grid item xs={7}>
+          <Grid container spacing="5px" direction="column" alignItems="left">
+            <Grid item>
+              <MoodSelector
+                id={props.id}
+                getUserMoods={getUserMoods}
+                setMoodSelected={setMoodSelected}
+              />
+            </Grid>
+            <Grid item>
+              <PulseOverview
+                todaysDateState={props.todaysDateState}
+                id={props.id}
+                userMoods={userMoods}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* this grid item contains today's calendar and notiifcations. */}
+        <Grid item xs={5}>
+          <Grid container spacing="5px" direction="column" alignItems="center">
+            <Grid item>
+              <DateCard todaysDateState={props.todaysDateState} />
+            </Grid>
+            <Grid item>
+              <Notifications />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/components/Dashboard/PulseOverview/ReportCard.jsx
+++ src/components/Dashboard/PulseOverview/ReportCard.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import {
-	Card,
-	CardContent,
-	CardActions,
-	Typography,
-	Button,
-	Box,
-	Grid,
-	Fab,
+  Card,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  Box,
+  Grid,
+  Fab,
+  Container,
+  Avatar,
 } from "@mui/material";
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied";
 import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied";
@@ -16,42 +18,54 @@ import SentimentSatisfiedIcon from "@mui/icons-material/SentimentSatisfied";
 import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 
 const ReportCard = (props) => {
-	const startIndex = props.moodData.length - 4;
-	const endIndex = props.moodData.length - 1;
-	const selectedElements = props.moodData.slice(startIndex, endIndex);
+  const startIndex = props.moodData.length - 4;
+  const endIndex = props.moodData.length - 1;
+  const selectedElements = props.moodData.slice(startIndex, endIndex);
 
-	const moodsArray = selectedElements.map((element) => element.mood);
-	const sum = moodsArray.reduce((acc, current) => acc + current, 0);
-	const average = sum / moodsArray.length;
+  const moodsArray = selectedElements.map((element) => element.mood);
+  const sum = moodsArray.reduce((acc, current) => acc + current, 0);
+  const average = sum / moodsArray.length;
 
-	let moodValue = "";
+  let moodValue = "";
+  let bgColor;
 
-	if (average >= 0 && average <= 2) {
-		moodValue = "sad";
-	} else if (average > 2 && average <= 3.5) {
-		moodValue = "okay";
-	} else {
-		moodValue = "happy";
-	}
+  if (average >= 0 && average <= 2) {
+    moodValue = "sad";
+    bgColor = "smiley1Backing.main";
+  } else if (average > 2 && average <= 3.5) {
+    moodValue = "okay";
+    bgColor = "smiley3Backing.main";
+  } else {
+    moodValue = "happy";
+    bgColor = "smiley5Backing.main";
+  }
 
-	return (
-		<Box
-			sx={{
-				backgroundColor: "smiley5Backing.main",
-				borderRadius: "8px",
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: bgColor,
+          borderRadius: "8px",
 
-				padding: "16px",
-				width: "100%",
-				height: "33%",
-			}}
-		>
-			<Typography variant="h6">{props.name}</Typography>
-			<Typography variant="h6">{props.title}</Typography>
-			<Typography variant="body1">
-				{props.name} has been feeling {moodValue} for the past 3 days.
-			</Typography>
-		</Box>
-	);
+          padding: "16px",
+          width: "100%",
+          height: "33%",
+        }}
+      >
+        <Container sx={{ display: "flex" }}>
+          <Avatar src={`../../../../pictures/${props.profilePicture}`}></Avatar>
+          <Container>
+            <Typography variant="h6">{props.name}</Typography>
+            <Typography variant="h6">{props.title}</Typography>
+            <Typography variant="body1">
+              {props.name} has been feeling {moodValue} for the past 3 days.
+            </Typography>
+          </Container>
+        </Container>
+      </Box>
+      <br />
+    </>
+  );
 };
 
 export default ReportCard;
